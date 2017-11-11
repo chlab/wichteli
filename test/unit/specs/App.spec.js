f19b818fe22ccb5ede8442bb50ff65cee3937e0c
Christof Leuenberger <chris@chlab.ch>
2017-11-11 14:33:25 +0100

    Update the App.vue unit test
    - Test if the mutations are triggered that are supposed to be triggered
    - Remove child component testing as tests should be shallow

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -1,12 +1,30 @@
-import { mount } from 'avoriaz'
+import { createLocalVue, shallow } from 'vue-test-utils'
+import Vuex from 'vuex'
 import App from '@/App.vue'
-import store from '@/vuex/'
 
 describe('App.vue', () => {
-  it('should add and render a first participant', () => {
-    const wrapper = mount(App, {
+  // local vue since we're installing vuex plugin
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+
+  // setup mock store
+  const mutations = {
+    addNewParticipant: jest.fn(),
+    addNewException: jest.fn()
+  }
+  const store = new Vuex.Store({
+    mutations,
+    getters: { hasParticipants: jest.fn() }
+  })
+
+  it('should call the store to add inintial data before it\'s mounted', () => {
+    shallow(App, {
+      localVue,
       store
     })
-    expect(wrapper.find('.participant').length).toBeGreaterThanOrEqual(1)
+    expect(mutations.addNewParticipant).toHaveBeenCalledTimes(1)
+    expect(mutations.addNewException).toHaveBeenCalledTimes(1)
   })
+
+  // @todo add test to check if Exception list is added if there are participants
 })
