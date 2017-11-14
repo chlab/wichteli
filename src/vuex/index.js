52c64a4cf8be89d674a63ed4cc52e21dddce2da2
Christof Leuenberger <chris@chlab.ch>
2017-11-14 22:16:13 +0100

    Only add fake data on dev env

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -1,26 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import uniqueId from 'lodash.uniqueid'
+import reject from 'lodash.reject'
 
 import mutations from './mutations'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {
-    participants: [
-      { id: 'p-1', name: 'Thierry', email: 'thierry@mail.de' },
-      { id: 'p-2', name: 'Markus', email: 'markus@mail.de' },
-      { id: 'p-3', name: 'Tom', email: 'tom@mail.de' }
-    ],
-
+    participants: [],
     exceptions: []
   },
 
   getters: {
     participantById: state => id => state.participants.find(p => p.id === id),
     exceptionById: state => id => state.exceptions.find(e => e.id === id),
-    hasParticipants: state => state.participants.length > 0
+    hasParticipants: state => state.participants.length > 0,
+    validParticipants: state => reject(state.participants, p => {
+      return p.name === '' || p.email === ''
+    })
   },
 
   mutations
 })
+
+// add some participants on dev
+if (process.env.NODE_ENV === 'development') {
+  store.state.participants.push({ id: uniqueId('p'), name: 'Thierry', email: 'thierry@mail.de' })
+  store.state.participants.push({ id: uniqueId('p'), name: 'Markus', email: 'markus@mail.de' })
+  store.state.participants.push({ id: uniqueId('p'), name: 'Tom', email: 'tom@mail.de' })
+}
+
+export default store
